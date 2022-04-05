cc7c04917874a1e94d29f97e52cb69663beaf5fe
lakshmipriyamurugesan <lpmlakshmipriya@gmail.com>
2022-04-05 19:41:34 +0530

    lakshmi code

---
+++ src/test/java/com/cts/Class1.java
@@ -0,0 +1,17 @@
+package com.cts;
+
+public class Class1 {
+	public void method1() {
+		System.out.println("Method 1");
+
+	}
+	public void method2() {
+		System.out.println("Method 2");
+
+	}
+	public void method3() {
+		System.out.println("Method 3");
+
+	}
+
+}
